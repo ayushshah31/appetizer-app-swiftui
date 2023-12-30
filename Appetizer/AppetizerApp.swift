85040dd8f689ad13d3ec37efbbcd93c49985e729
Ayush Shah <shahayush934@gmail.com>
2023-12-30 22:19:52 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by ayush on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
